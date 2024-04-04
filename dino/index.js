932589b24a5d10219d121b5f615ff2b59eb3d138
Vazgen <norvardyanvazgen@gmail.com>
2024-04-04 18:30:48 +0300

    dino game

--- dino/index.js
+++ dino/index.js
@@ -1,8 +1,16 @@
-// Получаем контекст рисования на канвасе
+const scoreSpan = document.querySelector('.score span');
+const dialog = document.querySelector('.dialog');
+const professionSpan = document.querySelector('.profession');
+const playAgainBtn = document.querySelector('.play-again');
+
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
-let = running = true;
-const gravity = 1;
+let running = true;
+
+const gravity = 0.7;
+const speed = 5;
+const jumpStrength = 20;
+const chance = 0.01; // 0.01
 
 const professions = [
     'программист',
@@ -11,10 +19,27 @@ const professions = [
     'электромонтажник',
 ]
 
+const professionsImgPath = [
+    "assets/prog.svg",
+    "assets/designer.svg",
+    "assets/electro.svg",
+    "assets/screwdriver.svg",
+]
+
+const professionsImgs = [
+    new Image(),
+    new Image(),
+    new Image(),
+    new Image(),
+];
+
 const startPage = document.querySelector('.start-page');
 let profession;
+const dinoImg = new Image();
 
-
+playAgainBtn.onclick = () => {
+    location.reload();
+}
 
 class Vec2 {
     constructor(x, y) {
@@ -26,75 +51,89 @@ class Vec2 {
 class Dino {
     /**
      * Initializes a new instance of the class with the specified position and size.
-     *
-     * @param {number} x - The x-coordinate of the position.
-     * @param {number} y - The y-coordinate of the position.
-     * @param {number} width - The width of the size.
-     * @param {number} height - The height of the size.
-     */
-    constructor(x, y, width, height) {
-        this.pos = new Vec2(x, y);
-        this.vel = new Vec2(0, 0);
-        this.size = new Vec2(width, height);
-        this.onGround = true;
+    *
+    * @param {number} x - The x-coordinate of the position.
+    * @param {number} y - The y-coordinate of the position.
+    * @param {number} width - The width of the size.
+    * @param {number} height - The height of the size.
+    */
+   constructor(x, y, width, height) {
+       this.pos = new Vec2(x, y);
+       this.vel = new Vec2(0, 0);
+       this.size = new Vec2(width, height);
+       this.onGround = true;
     }
 }
 
 class Obstacle {
-    constructor(x, y) {
+    constructor(x, y, profession) {
         this.pos = new Vec2(x, y);
-        this.size = new Vec2(20, 50);
-        this.profession = 0
+        this.size = new Vec2(50, 50);
+        this.profession = profession;
     }
 }
 
-// Объявляем переменные для динозавра и препятствий
-// let dinoX = 50;
-// let dinoY = canvas.height - 50;
-const dino = new Dino(50, canvas.height - 50, 50, 50);
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 
-// const dinoWidth = 50;
-// const dinoHeight = 50;
+const dino = new Dino(50, canvas.height - 50, 80, 80);
+
+/** @type {Obstacle[]} */
 let obstacles = [];
 
-// Функция для отрисовки динозавра
 function drawDino() {
-    ctx.fillStyle = 'green';
-    ctx.fillRect(dino.pos.x, dino.pos.y, dino.size.x, dino.size.y);
+    ctx.drawImage(dinoImg, dino.pos.x, dino.pos.y, dino.size.x, dino.size.y);
 }
 
-// game over
 function gameOver() {
     running = false;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.font = "30px Arial";
-    ctx.fillText('Игра окончена!', canvas.width / 2, canvas.height / 2);
-    // alert('Игра окончена!');
-    // location.reload(); // Перезагружаем страницу
+    
+    dialog.oncancel = e => e.preventDefault();
+    dialog.showModal();
 }
 
-// Функция для отрисовки препятствий
 function drawObstacles() {
     ctx.fillStyle = 'brown';
     for (const obstacle of obstacles) {
-        ctx.fillRect(obstacle.pos.x, obstacle.pos.y, obstacle.size.x, obstacle.size.y);
-        obstacle.pos.x -= 5; // Двигаем препятствие влево
+        ctx.drawImage(professionsImgs[obstacle.profession], obstacle.pos.x, obstacle.pos.y, obstacle.size.x, obstacle.size.y);
+        obstacle.pos.x -= speed; // obstacle.pos.x -= 5;
     }
 }
 
+let score = 0;
+function increaseScore() {
+    score += 10;
+    scoreSpan.textContent = `${score}%`;
+    if (score >= 100) {
+        gameOver();
+    }
+}
+
+function decreaseScore() {
+    if (score <= 0) return;
+    score -= 5;
+    scoreSpan.textContent = `${score}%`;
+}
+
 function checkCollisions() {
-    for (const obstacle of obstacles) {
-        if (collideCheck(dino.pos, obstacle.pos, dino.size, obstacle.size)) {
-            gameOver();
+    for (const [i, obstacle] of obstacles.entries()) {
+        if (collideCheck(dino.pos, dino.size, obstacle.pos, obstacle.size)) {
+            
+            if (obstacle.profession == profession)
+                increaseScore();
+            else
+                decreaseScore();
+
+            obstacles.splice(i, 1);
             break;
         }
     }
 }
 
-// Функция для прыжка динозавра
 function jump() {
     if (!dino.onGround) return;
-    dino.vel.y -= 20;
+    dino.vel.y -= jumpStrength;
     dino.onGround = false;
 }
 
@@ -106,7 +145,7 @@ document.addEventListener('keydown', (event) => {
 });
 
 
-function collideCheck(vec1, vec2, vec1size, vec2size) {
+function collideCheck(vec1, vec1size, vec2, vec2size) {
     // Half sizes for easier calculation
     const halfSize1 = { x: vec1size.x / 2, y: vec1size.y / 2 };
     const halfSize2 = { x: vec2size.x / 2, y: vec2size.y / 2 };
@@ -125,37 +164,50 @@ function applyGravity() {
     if (!running) return;
     dino.pos.y += dino.vel.y;
     dino.vel.y += gravity;
-    if (dino.pos.y > canvas.height - dino.size.y) {
+    if (dino.pos.y >= canvas.height - dino.size.y) {
         dino.onGround = true;
         dino.vel.y = 0;
         dino.pos.y = canvas.height - dino.size.y;
     }
 }
 
-// Главная функция игрового цикла
+function clearGarbage() {
+    obstacles = obstacles.filter(obstacle => (obstacle.pos.x + obstacle.size.x) > 0);
+}
+
 function gameLoop() {
-    // Очищаем канвас
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Создаем новое препятствие с определенной вероятностью
-    if (Math.random() < 0.01) {
-        // obstacles.push({ x: canvas.width, y: canvas.height - 70 });
-        obstacles.push(new Obstacle(canvas.width, canvas.height - 50));
+    if (Math.random() < chance) {
+        obstacles.push(new Obstacle(canvas.width, [canvas.height-200, canvas.height - 50][getRandomInt(2)], getRandomInt(4)));
     }
 
-    // Отрисовываем динозавра и препятствия
     applyGravity();
     drawDino();
     drawObstacles();
     checkCollisions();
+    clearGarbage();
     if (!running) return;
-    // Запускаем следующий кадр
+
     requestAnimationFrame(gameLoop);
 }
 
+zip = (...rows) => [...rows[0]].map((_,c) => rows.map(row => row[c]))
+
+function startGame() {
+    for (const [imgPath, professionsImg] of zip(professionsImgPath, professionsImgs)) {
+        professionsImg.src = imgPath;
+    }
+    dinoImg.src = 'assets/dino.svg';
+    dinoImg.onload = () => {
+        gameLoop();
+    };
+}
+
 function selectProfession(prof) {
-    profession = prof
+    professionSpan.textContent = professions[prof];
+    profession = prof;
     console.log(professions[profession])
     startPage.style.display = 'none';
-    gameLoop();
+    startGame();
 }
